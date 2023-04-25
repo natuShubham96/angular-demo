0069eca377cfc28379c19b2e9954f7cfef800710
Shubham Natu <shubhamnatu@MSPLCHMA000213.local>
2023-04-25 16:33:12 +0530

    Creating Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from "./Server/server.component";
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import {SuccessAlertComponent} from "./success-alert/success-alert.component";
+import {AlertsComponent} from "./Alerts/alerts.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    AlertsComponent
   ],
   imports: [
     BrowserModule
