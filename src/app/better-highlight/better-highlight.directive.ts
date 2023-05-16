72f44f319879a5fc7eeb2e0f5e3bee0a2e7124b6
Shubham Natu <shubhamnatu@MSPLCHMA000213.local>
2023-05-16 16:30:37 +0530

    Using custom directives with property binding to pass down data to directive

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,24 +1,27 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'yellow'
+  @Input() defaultColor: string;
+  @Input() highlightColor: string;
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background', 'red')
   }
 
   @HostListener('mouseenter') mouseOver(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background', 'red')
-    this.backgroundColor = 'red';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background', 'green')
-    this.backgroundColor = 'green';
+    this.backgroundColor = this.defaultColor;
   }
 }
